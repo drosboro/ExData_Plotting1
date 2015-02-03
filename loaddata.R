afb14582737fc6431d369ada5ea2c99f7ef12dbb
Dave Rosborough <dave@rosboroughtech.com>
2015-02-02 22:07:10 -0800

    checks first for filtered data set before downloading raw data

--- loaddata.R
+++ loaddata.R
@@ -1,16 +1,17 @@
 # Require data file to exist in working directory
-if (!file.exists("household_power_consumption.txt")) {
-  try(download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
-                "household_power_consumption.zip", method="curl"))
-  try(system("unzip household_power_consumption.zip"))
+if (!file.exists("hpc_filtered.txt")) {
   
   if (!file.exists("household_power_consumption.txt")) {
-    stop("Data file (household_power_consumption.txt) doesn't exist in working directory")
+    try(download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
+                      "household_power_consumption.zip", method="curl"))
+    try(system("unzip household_power_consumption.zip"))
+    
+    if (!file.exists("household_power_consumption.txt")) {
+      stop("Data file (household_power_consumption.txt) doesn't exist in working directory")
+    }
   }
-}
 
 # Create filtered file containing only dates of interest, if it doesn't exist
-if (!file.exists("hpc_filtered.txt")) {
   try(system("head -n 1 household_power_consumption.txt > hpc_filtered.txt"))
   try(system("grep '^[12]/2/2007;' household_power_consumption.txt >> hpc_filtered.txt"))
   
